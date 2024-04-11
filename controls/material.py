cad6fc7312a89175d9fab67f33cb50080bc75f00
thetrotfreak <thetrotfreak@yahoo.com>
2024-04-11 18:06:56 +0530

    fix: real-time preference update of accent color
    Use a container for accent chooser
    
    Store a ref to active container & ResponsiveRow of Containers
    
    Use centered titles in AlertDialog
    
    Refs: #3

--- controls/material.py
+++ controls/material.py
@@ -1,17 +1,18 @@
 from flet import (
     AlertDialog,
-    ButtonStyle,
-    CircleBorder,
+    BoxShape,
+    Container,
     ControlEvent,
     Dropdown,
-    ElevatedButton,
-    IconButton,
+    Icon,
     Page,
     Ref,
     ResponsiveRow,
     Text,
+    TextAlign,
     TextThemeStyle,
     UserControl,
+    colors,
     icons,
 )
 
@@ -24,50 +25,82 @@ class MaterialYouCustomizationDialog(UserControl):
         self.preference = Preference(page)
         self.page = page
         self.dropdown = Ref[Dropdown]()
+        self._color_clickable = Ref[ResponsiveRow]()
+        self._active_container = Ref[Container]()
         self.dialog = AlertDialog(
             title=Text(
                 value="material accent".title(),
                 theme_style=TextThemeStyle.TITLE_LARGE,
+                expand=True,
+                text_align=TextAlign.CENTER,
             ),
             content=ResponsiveRow(
-                controls=[*map(lambda color: self.button(color), Preference.__COLORS__)]
+                ref=self._color_clickable,
+                controls=[
+                    *map(lambda color: self.button(color), Preference.__COLORS__)
+                ],
             ),
         )
 
-    def button(self, color: str) -> IconButton | ElevatedButton:
-        if color.casefold() == self.page.theme.color_scheme_seed.casefold():
-            return IconButton(
-                icon=icons.DONE,
-                col=2,
-                bgcolor=color,
-                style=ButtonStyle(shape=CircleBorder()),
-                tooltip=color.title(),
-                on_click=lambda event: self.__accent__(event),
-            )
-        else:
-            return ElevatedButton(
-                col=2,
-                bgcolor=color,
-                style=ButtonStyle(shape=CircleBorder()),
-                tooltip=color.title(),
-                on_click=lambda event: self.__accent__(event),
-            )
+    def __on_click__(self, event: ControlEvent):
+        # remove icon from current accent
+        self._active_container.current.content = None
+        self._active_container.current.update()
 
-    def __accent__(self, event: ControlEvent):
-        if isinstance(event.control, IconButton):
-            pass
-        else:
-            event.control = IconButton(
-                icon=icons.DONE,
-                col=2,
-                bgcolor=event.control.bgcolor,
-                style=ButtonStyle(shape=CircleBorder()),
-                tooltip=event.control.bgcolor.title(),
-                on_click=lambda event: self.__accent__(event),
-            )
-            self.preference.update(k="accent", v=event.control.bgcolor)
-            event.control.update()
-            self.page.update()
+        # iterate over all color containers
+        # updating only the clicked color container
+        for index, container in enumerate(self._color_clickable.current.controls):
+            if container.key == event.control.key:
+                self._color_clickable.current.controls[index] = Container(
+                    ref=self._active_container,
+                    key=event.control.key,
+                    content=Icon(
+                        name=icons.DONE,
+                        color=colors.ON_PRIMARY,
+                    ),
+                    bgcolor=event.control.bgcolor,
+                    width=24 * 2,
+                    height=24 * 2,
+                    col=2,
+                    shape=BoxShape.CIRCLE,
+                    on_click=lambda e: self.__on_click__(event=e),
+                    expand=False,
+                    shadow=None,
+                    ink=False,
+                    tooltip=event.control.bgcolor.title(),
+                )
+                break
+        self.preference.update(k="accent", v=event.control.bgcolor)
+        self._color_clickable.current.update()
+        self.page.update()
+
+    def button(self, color: str) -> Container:
+        return Container(
+            key=color,
+            ref=(
+                self._active_container
+                if self.page.theme.color_scheme_seed.casefold() == color.casefold()
+                else None
+            ),
+            content=(
+                Icon(
+                    name=icons.DONE,
+                    color=colors.ON_PRIMARY,
+                )
+                if self.page.theme.color_scheme_seed.casefold() == color.casefold()
+                else None
+            ),
+            bgcolor=color,
+            width=24 * 2,
+            height=24 * 2,
+            col=2,
+            shape=BoxShape.CIRCLE,
+            on_click=lambda event: self.__on_click__(event=event),
+            expand=False,
+            shadow=None,
+            ink=False,
+            tooltip=color.title(),
+        )
 
     def open(self):
         self.page.dialog = self.dialog
--- controls/theme.py
+++ controls/theme.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from flet import (
     AlertDialog,
     Column,
@@ -8,6 +10,7 @@ from flet import (
     Radio,
     RadioGroup,
     Text,
+    TextAlign,
     TextThemeStyle,
     ThemeMode,
     UserControl,
@@ -17,14 +20,17 @@ from utils import Preference
 
 
 class ChooseThemeDialog(UserControl):
-    def __init__(self, page: Page):
+    def __init__(self, page: Page, callback: Callable[[], None]):
         super().__init__()
         self.pref = Preference(page)
         self.page = page
+        self.callback = callback
         self.dialog = AlertDialog(
             title=Text(
-                value="choose theme".title(),
+                value="app theme".title(),
                 theme_style=TextThemeStyle.TITLE_LARGE,
+                expand=True,
+                text_align=TextAlign.CENTER,
             ),
             content=Column(
                 controls=[
@@ -51,7 +57,8 @@ class ChooseThemeDialog(UserControl):
 
     def __theme_mode__(self, event: ControlEvent):
         self.pref.update(k="theme", v=event.control.value)
-        self.page.theme_mode
+        self.page.theme_mode = event.control.value
+        self.callback()
 
     def open(self):
         self.page.dialog = self.dialog
